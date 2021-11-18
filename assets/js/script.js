4e3126750d9461c2d79fa8a78620692498d65ba0
Isaque Silva <79009221+ZekaBoga@users.noreply.github.com>
2021-11-18 08:39:56 -0300

    even better code for sure!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-api = "https://swapi.dev/api/";
+const api = "https://swapi.dev/api/";
 
+const getInfo = async (object, id) => {response = await fetch(`${api}${object}/${id}`);return response.json();}
 // ===== People Info ===== //
 // Luke Skywalker
-async function lukeInfo() {
-    let response = await fetch(`${api}people/1`)
-    return response.json();
-}
-lukeInfo().then(data => {
+getInfo('people', 1).then(data => { 
     document.querySelector('#luke1').innerHTML = data.name;
     document.querySelector('#luke2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#luke3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -15,11 +12,7 @@ lukeInfo().then(data => {
 });
 
 // Darth Vader
-async function darthInfo() {
-    let response = await fetch(`${api}people/4`)
-    return response.json();
-}
-darthInfo().then(data => {
+getInfo('people', 4).then(data => { 
     document.querySelector('#vader1').innerHTML = data.name;
     document.querySelector('#vader2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#vader3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -28,11 +21,7 @@ darthInfo().then(data => {
 });
 
 // Leia Organa
-async function leiaInfo() {
-    let response = await fetch(`${api}people/5`)
-    return response.json();
-}
-leiaInfo().then(data => {
+getInfo('people', 5).then(data => { 
     document.querySelector('#leia1').innerHTML = data.name;
     document.querySelector('#leia2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#leia3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -41,11 +30,7 @@ leiaInfo().then(data => {
 });
 
 // Owen Lar
-async function owenInfo() {
-    let response = await fetch(`${api}people/6`)
-    return response.json();
-}
-owenInfo().then(data => {
+getInfo('people', 6).then(data => { 
     document.querySelector('#Owen1').innerHTML = data.name;
     document.querySelector('#Owen2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#Owen3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -54,11 +39,7 @@ owenInfo().then(data => {
 });
 
 // Beru Whitesun
-async function beruInfo() {
-    let response = await fetch(`${api}people/7`)
-    return response.json();
-}
-beruInfo().then(data => {
+getInfo('people', 7).then(data => { 
     document.querySelector('#Beru1').innerHTML = data.name;
     document.querySelector('#Beru2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#Beru3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -67,11 +48,7 @@ beruInfo().then(data => {
 });
 
 // Obi-Wan Kenobi
-async function obiInfo() {
-    let response = await fetch(`${api}people/10`)
-    return response.json();
-}
-obiInfo().then(data => {
+getInfo('people', 10).then(data => { 
     document.querySelector('#Obi1').innerHTML = `${data.name}`;
     document.querySelector('#Obi2').innerHTML = `height: <span>${data.height}cm`;
     document.querySelector('#Obi3').innerHTML = `skin color: <span>${data.skin_color}`;
@@ -81,11 +58,7 @@ obiInfo().then(data => {
 
 // ===== Movies ===== //
 // A New Hope
-async function film1Info() {
-    let response = await fetch(`${api}films/1`)
-    return response.json();
-}
-film1Info().then(data => {
+getInfo('films', 1).then(data => { 
     document.querySelector('#movie1').innerHTML = data.title;
     document.querySelector('#movie2').innerHTML = `release date: <span>${data.release_date}`;
     document.querySelector('#movie3').innerHTML = `rroducer: <span>${data.producer}`;
@@ -94,11 +67,7 @@ film1Info().then(data => {
 });
 
 // The Empire Strikes Back
-async function film2Info() {
-    let response = await fetch(`${api}films/2`)
-    return response.json();
-}
-film2Info().then(data => {
+getInfo('films', 2).then(data => { 
     document.querySelector('#movie01').innerHTML = data.title;
     document.querySelector('#movie02').innerHTML = `release date: <span>${data.release_date}`;
     document.querySelector('#movie03').innerHTML = `rroducer: <span>${data.producer}`;
@@ -107,11 +76,7 @@ film2Info().then(data => {
 });
 
 // Return of the Jedi
-async function film3Info() {
-    let response = await fetch(`${api}films/3`)
-    return response.json();
-}
-film3Info().then(data => {
+getInfo('films', 3).then(data => { 
     document.querySelector('#movie001').innerHTML = data.title;
     document.querySelector('#movie002').innerHTML = `release date: <span>${data.release_date}`;
     document.querySelector('#movie003').innerHTML = `rroducer: <span>${data.producer}`;
@@ -121,72 +86,44 @@ film3Info().then(data => {
 
 // ===== Species ===== //
 // Human
-async function humanInfo() {
-    let response = await fetch(`${api}species/1`)
-    return response.json();
-}
-async function humanPlanet() {
-    let response = await fetch(`${api}planets/9`)
-    return response.json();
-}
-humanInfo().then(data => {
+getInfo('species', 1).then(data => { 
     document.querySelector('#species1').innerHTML = data.name;
     document.querySelector('#species2').innerHTML = `classification: <span>${data.classification}`;
     document.querySelector('#species3').innerHTML = `average lifespan: <span>${data.average_lifespan}`;
     document.querySelector('#species4').innerHTML = `language: <span>${data.language}`;
 
-    humanPlanet().then(data => {
+    getInfo('planets', 9).then(data => { 
         document.querySelector('#species5').innerHTML = `homeworld: <span>${data.name}`;
     });
 });
 
 // Wookie
-async function wookieInfo() {
-    let response = await fetch(`${api}species/3`)
-    return response.json();
-}
-async function wookiePlanet() {
-    let response = await fetch(`${api}planets/9`)
-    return response.json();
-}
-wookieInfo().then(data => {
+getInfo('species', 3).then(data => { 
     document.querySelector('#species01').innerHTML = data.name;
     document.querySelector('#species02').innerHTML = `classification: <span>${data.classification}`;
     document.querySelector('#species03').innerHTML = `average lifespan: <span>${data.average_lifespan}`;
     document.querySelector('#species04').innerHTML = `language: <span>${data.language}`;
 
-    wookiePlanet().then(data => {
+    getInfo('planets', 14).then(data => { 
         document.querySelector('#species05').innerHTML = `homeworld: <span>${data.name}`;
     });
 });
 
 // Rodian
-async function rodianInfo() {
-    let response = await fetch(`${api}species/4`)
-    return response.json();
-}
-async function rodianPlanet() {
-    let response = await fetch(`${api}planets/14`)
-    return response.json();
-}
-rodianInfo().then(data => {
+getInfo('species', 4).then(data => { 
     document.querySelector('#species001').innerHTML = data.name;
     document.querySelector('#species002').innerHTML = `classification: <span>${data.classification}`;
     document.querySelector('#species003').innerHTML = `average lifespan: <span>${data.average_lifespan}`;
     document.querySelector('#species004').innerHTML = `language: <span>${data.language}`;
 
-    rodianPlanet().then(data => {
+    getInfo('planets', 23).then(data => { 
         document.querySelector('#species005').innerHTML = `homeworld: <span>${data.name}`;
     });
 });
 
 // ===== Starships ===== //
 // CR90 corvette
-async function ship1Info() {
-    let response = await fetch(`${api}starships/2`)
-    return response.json();
-}
-ship1Info().then(data => {
+getInfo('starships', 2).then(data => { 
     document.querySelector('#starships1').innerHTML = data.name;
     document.querySelector('#starships2').innerHTML = `manufacturer: <span>${data.manufacturer}`;
     document.querySelector('#starships3').innerHTML = `cost: <span>${data.cost_in_credits}`;
@@ -195,11 +132,7 @@ ship1Info().then(data => {
 });
 
 // Star Destroyer
-async function ship2Info() {
-    let response = await fetch(`${api}starships/3`)
-    return response.json();
-}
-ship2Info().then(data => {
+getInfo('starships', 3).then(data => { 
     document.querySelector('#starships01').innerHTML = data.name;
     document.querySelector('#starships02').innerHTML = `manufacturer: <span>${data.manufacturer}`;
     document.querySelector('#starships03').innerHTML = `cost: <span>${data.cost_in_credits}`;
@@ -208,11 +141,7 @@ ship2Info().then(data => {
 });
 
 // Death Star
-async function ship3Info() {
-    let response = await fetch(`${api}starships/9`)
-    return response.json();
-}
-ship3Info().then(data => {
+getInfo('starships', 9).then(data => { 
     document.querySelector('#starships001').innerHTML = data.name;
     document.querySelector('#starships002').innerHTML = `manufacturer: <span>${data.manufacturer}`;
     document.querySelector('#starships003').innerHTML = `cost: <span>${data.cost_in_credits}`;
